0de1733f0db88ef76cf53ff25044580be05fd05f
kunasyl <kunasyl45@gmail.com>
2023-05-14 17:42:58 +0600

    fixed security issues

--- Neobis_Cinematica/settings.py
+++ Neobis_Cinematica/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-10&7gfszmuy_32hj(-aace4xsr0i#od@zynqhj-i%meg&my-=i'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
