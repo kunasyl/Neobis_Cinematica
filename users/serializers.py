35b820864e653e5ee97ea286c647e0ea9d7457ae
kunasyl <kunasyl45@gmail.com>
2023-05-14 17:37:40 +0600

    added authorization/registration

---
+++ users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password')
+
+
+class CreateTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+
+class GetUserSerializer(serializers.Serializer):
+    token = serializers.CharField()
